14c6f75b5e0c91f1011ff679bdf26233cd594211
Alex Grobovsky <ktotaam@mail.ru>
2021-06-15 00:34:16 +0500

    save

--- mrdonalds/src/App.js
+++ mrdonalds/src/App.js
@@ -1,10 +1,61 @@
 import React from 'react';
+// import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import NavBar from "./Components/NavBar/Navbar";
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+  
+  body {
+    background-color: #000;
+    color: #fff;
+    margin: 0;
+    font-size: 20px;
+    font-family: Roboto, sans-serif;
+  }
+  
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+  
+  h1, h2, h3 {
+    font-family: Pacifico, sans-serif;
+    padding: 0;
+    margin: 0;
+  }
+  
+  p {
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 const App = props => {
     return (
-        <div>
-            hello react
-        </div>
+        <React.Fragment>
+            <GlobalStyles />
+            <NavBar />
+        </React.Fragment>
     );
 
 };
